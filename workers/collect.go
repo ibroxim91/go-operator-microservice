8cc0bcffaf395fa0572db1aba74a1bd71cc9c4e7
ibroxim <ibroxim.rashidov9199@gmail.com>
2026-05-06 18:22:01 +0500

    Update ticket and price models

--- workers/collect.go
+++ workers/collect.go
@@ -3,44 +3,46 @@ package workers
 import (
 	"context"
 	"go-operator-service/models"
+	"log"
 	"sync"
 )
 
 func CollectResults(ctx context.Context, jobsList []models.Request, workerCount int) models.ResultResponse {
-    jobs := make(chan models.Request, len(jobsList))
-    results := make(chan models.Result, len(jobsList))
+	jobs := make(chan models.Request, len(jobsList))
+	results := make(chan models.Result, len(jobsList))
 
-    var wg sync.WaitGroup
-    for i := 0; i < workerCount; i++ {
-        wg.Add(1)
-        go worker(ctx, jobs, results, &wg)
-    }
+	var wg sync.WaitGroup
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		go worker(ctx, jobs, results, &wg)
+	}
 
-    for _, job := range jobsList {
-        select {
-        case <-ctx.Done():
-            break
-        case jobs <- job:
-        }
-    }
-    close(jobs)
+	for _, job := range jobsList {
+		select {
+		case <-ctx.Done():
+			break
+		case jobs <- job:
+		}
+	}
+	close(jobs)
 
-    go func() {
-        wg.Wait()
-        close(results)
-    }()
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
 
-    var allResults []*models.Ticket
-    total := 0
-  
-    page := jobsList[0].Page
-    for res := range results {
-        if len(res.Prices) == 100 {
-            total += res.Pager.Total * 100
-        } else if len(res.Prices) == 200 {
-            total += res.Pager.Total * 200
-        }
-        allResults = append(allResults, res.Prices...)
-    }
-    return models.ResultResponse{Prices: allResults, Total: total, Page: page}
+	var allResults []*models.Ticket
+	total := 0
+
+	page := jobsList[0].Page
+	for res := range results {
+		if len(res.Prices) == 100 {
+			total += res.Pager.Total * 100
+		} else if len(res.Prices) == 200 {
+			total += res.Pager.Total * 200
+		}
+		allResults = append(allResults, res.Prices...)
+	}
+	log.Println("REsults len ", len(allResults))
+	return models.ResultResponse{Prices: allResults, Total: total, Page: page}
 }
